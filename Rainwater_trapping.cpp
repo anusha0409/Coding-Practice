b1daa8cd1363b1d659332ab01a7f5d71d4a8e03d
anusha <anushaagarwal2000.com>
2020-07-02 16:12:21 +0530

    initial commit

---
+++ Rainwater_trapping.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+void trap(vector<int> height) {
+        //min(maxL,maxR)
+        //suffix[i]=max(A[i to n-1])
+        //running maximum from 0 to i-1
+        //subtact height of the i from the difference of the maximum
+       
+        
+        int n=height.size();
+        int i,maxSoFar=0,h,ans=0;
+        vector <int> suffix(n,0);
+        suffix[n-1]=height[n-1];
+        maxSoFar=height[n-1];
+        for(i=n-2;i>=0;i--)
+        {
+            suffix[i]=max(height[i],maxSoFar);
+            maxSoFar=max(suffix[i],maxSoFar);
+        }
+        maxSoFar=0;
+        for(i=0;i<n;i++)
+        	cout<<suffix[i]<<" ";
+        cout<<endl;
+         for(i=0;i<n-1;i++)
+        {
+           h=min(maxSoFar,suffix[i+1]);
+            maxSoFar=max(maxSoFar,height[i]);
+            if(h>=height[i])
+            {  h=h-height[i];
+            	cout<<h<<" ";
+            ans=ans+h;}
+            else cout<<0<<" ";
+        }
+        
+
+}
+int main(){
+
+ trap({3,0,2,0,4});
+
+  }
